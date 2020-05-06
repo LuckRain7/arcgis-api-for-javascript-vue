b81732d1789b4a44dd013a8bc6549866fcc0c603
LuckRain <763839938@qq.com>
2020-05-06 23:02:19 +0800

    update ant design vue

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,5 +1,14 @@
+/*
+ *  Description:
+ *  Author: LuckRain7
+ *  Date: 2020-05-06 11:10:22
+ */
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+  presets: ["@vue/cli-plugin-babel/preset"],
+  // plugins: [
+  //   [
+  //     "import",
+  //     { libraryName: "ant-design-vue", libraryDirectory: "es", style: true },
+  //   ],
+  // ],
+};
