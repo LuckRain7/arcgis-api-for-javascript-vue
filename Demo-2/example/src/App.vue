7efd13e200fb24799cbce3d5473305e10899e716
LuckRain7 <763839938@qq.com>
2020-12-15 10:47:49 +0800

    DEMO2:update

--- Demo-2/example/src/App.vue
+++ Demo-2/example/src/App.vue
@@ -9,8 +9,8 @@
 
 <script>
 import Header from "@/components/Header.vue"; // 引入头部组件
-import { loadModules, loadCss } from "esri-loader"; // 异步加载模块
-import config from "@/map/config.js"; // 配置项
+import ArcGIS from "@/map/init.js";
+const arcgis = new ArcGIS();
 
 export default {
   name: "App",
@@ -20,65 +20,7 @@ export default {
   },
 
   mounted() {
-    // 加载地图必备样式文件
-    loadCss("http://localhost:3000/arcgis-3.32/esri/css/esri.css");
-    loadCss("http://localhost:3000/arcgis-3.32/dijit/themes/claro/claro.css");
-
-    // 异步加载对应 js 模块
-    loadModules(
-      [
-        "esri/map",
-        "tdlib/SDTDTLayer",
-        "tdlib/SDRasterLayer",
-        "tdlib/SDRSAnnoLayer",
-        "esri/geometry/Extent",
-        "esri/SpatialReference",
-        "dojo/parser",
-      ],
-      config.loadConfig
-    )
-      .then(
-        ([
-          Map, // 地图模块
-          SDTDTLayer, // 山东天地图矢量地图
-          SDRasterLayer, // 山东天地图影像地图
-          SDRSAnnoLayer, // 山东天地图影像地图注记
-          Extent, // 范围模块
-          SpatialReference, // 坐标系模块
-          Parser, // 样式解析模块
-        ]) => {
-          // 设置地图地图图层
-          let baseMap = {
-            vectorMap: new SDTDTLayer(), //矢量地图
-            rasterMap: new SDRasterLayer(), //影像地图
-            rasterMapAnnotation: new SDRSAnnoLayer(), //影像注记
-            type: 1, // 1 为矢量 | 2：影像
-          };
-
-          Parser.parse(); // 解析
-
-          // 设置初始化地图位置
-          const startExtent = new Extent(
-            ...config.startExtent,
-            new SpatialReference({ wkid: 4490 })
-          );
-
-          // 添加地图实例
-          let map = new Map("map", {
-            extent: startExtent, // 初始化位置
-            zoom: 10, // 缩放级别
-            logo: false, // esri logo
-            maxZoom: 18, // 最大缩放级别
-            sliderPosition: "bottom-right", // 缩小放大按钮位置
-          });
-
-          // 将图层添加到地图实例上
-          map.addLayer(baseMap.vectorMap, 0);
-        }
-      ) //end
-      .catch((err) => {
-        console.error(err);
-      });
+    arcgis.init("map");
   },
 };
 </script>
